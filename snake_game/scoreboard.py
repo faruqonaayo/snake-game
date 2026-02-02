7a04058f7d08a51b139ef8fe1b7c0016cf6c0e77
Faruq Ogunkunle <onaayofaruq24@gmail.com>
2026-02-02 07:38:28 -0600

    Add high score tracking and persistence using files.

---
+++ snake_game/scoreboard.py
@@ -0,0 +1,34 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 20, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.score = 0
+        with open("data.txt", "r") as f:
+            self.high_score = int(f.read()[0][0])
+        self.color("white")
+        self.hideturtle()
+        self.goto(0,270)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score = {self.score}, High Score: {self.high_score}", move=False, align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", "w") as f:
+                f.write(str(self.high_score))
+        self.score = 0
+        self.update_scoreboard()
